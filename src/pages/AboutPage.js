8b21991a14fcb25b4167c9e0e3d19b4e553835f8
fhonoria <83973848+fhonoria@users.noreply.github.com>
2023-08-10 14:34:42 +0200

    added scrolltotop

--- src/pages/AboutPage.js
+++ src/pages/AboutPage.js
@@ -2,10 +2,12 @@ import React from "react";
 import About from "../components/About";
 import Footer from "../components/Footer";
 import Navbar from "../components/Navbar";
+import ScrollToTop from "../components/ScrollToTop";
 
 const AboutPage = () => {
   return (
     <div>
+      <ScrollToTop />
       <Navbar />
       <About />
       <Footer />
--- src/pages/PackagesPage.js
+++ src/pages/PackagesPage.js
@@ -2,10 +2,12 @@ import React from "react";
 import Footer from "../components/Footer";
 import Navbar from "../components/Navbar";
 import Packages from "../components/Packages";
+import ScrollToTop from "../components/ScrollToTop";
 
 const PackagesPage = () => {
   return (
     <div>
+      <ScrollToTop />
       <Navbar />
       <Packages />
       <Footer />
